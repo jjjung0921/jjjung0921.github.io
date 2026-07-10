24dd561ffdd77991bfb887b1e7272b141b12f1f5
jjjung0921 <98745092+jjjung0921@users.noreply.github.com>
2026-07-10 16:07:15 +0900

    chore: add github pages ci cd

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
 
 export default defineConfig({
+  site: 'https://jjjung0921.github.io',
   markdown: {
     rehypePlugins: [rehypeKatex],
     remarkPlugins: [remarkMath],
