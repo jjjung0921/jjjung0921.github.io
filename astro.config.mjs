ca85f7f1c1147eeda1c7b34b33384f0854aca671
jjjung0921 <98745092+jjjung0921@users.noreply.github.com>
2026-07-19 15:10:09 +0900

    [Feat] makrdown 글머리 추가 && typescript everyday type post 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import remarkCallout from '@r4ai/remark-callout';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
 
@@ -6,7 +7,7 @@ export default defineConfig({
   site: 'https://jjjung0921.github.io',
   markdown: {
     rehypePlugins: [rehypeKatex],
-    remarkPlugins: [remarkMath],
+    remarkPlugins: [remarkMath, remarkCallout],
   },
   output: 'static',
 });
